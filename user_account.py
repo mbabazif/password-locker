257dce637f26375e4cfdce8c72ca28193c5e9268
Florence <mbababziflorence81@gmail.com>
2019-02-11 15:39:12 +0200

    adding the instance of the class  account

--- user_account.py
+++ user_account.py
@@ -2,12 +2,14 @@ class Account:
     """
     Class that generates new instances of account
     """
+    account_list = [] # Empty contact list
     def _int_(self,first_name,second_name,password):
          """
         Class that generates new instances of account
         """
         __init__ method that helps us define properties for our objects.
 
+        # docstring removed for simplicity
         self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
