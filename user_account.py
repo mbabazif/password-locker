e8e4032063b80f82f04298d872795379b9879a95
Florence <mbababziflorence81@gmail.com>
2019-02-11 15:26:04 +0200

    addind the class name account

---
+++ user_account.py
@@ -0,0 +1,13 @@
+class Account:
+    """
+    Class that generates new instances of account
+    """
+    def _int_(self,first_name,second_name,password):
+         """
+        Class that generates new instances of account
+        """
+        __init__ method that helps us define properties for our objects.
+
+        self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
